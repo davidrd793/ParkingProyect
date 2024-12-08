6906596be5e9416b715a616f4f6a6fb4d6100042
Davidrd79 <davidrodriguezdagas@gmail.com>
2024-12-08 19:40:41 +0000

    Some changes in vis.py

--- src/vis.py
+++ src/vis.py
@@ -4,7 +4,13 @@ class ParkingGUI:
     def __init__(self, root):
         self.root = root
         self.root.title = "Parking Visualization"
-        self.root.geometry("1000x700")
+        self.root.geometry("1000x700+300+150")
+
+        self.canvas = Canvas(self.root, width=200, height=200, bg="gray")
+        self.canvas.pack(pady = 50)
+        self.menu_frame = Frame(self.root, width=100, height=300, bg="lightblue")
+        self.menu_frame.pack()
+        boton = Button(self.menu_frame, text="Ver Plazas").grid(row=0, column=1, padx=10)
 
 
 if __name__=="__main__":
