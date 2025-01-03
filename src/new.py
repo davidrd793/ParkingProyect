40f5796fc4c9c1871529687f534bbc736e15b47d
Davidrd79 <davidrodriguezdagas@gmail.com>
2025-01-03 13:55:37 +0100

    No me acuerdo de que hice xd

--- src/new.py
+++ src/new.py
@@ -7,13 +7,11 @@ import time
 matriculas = ['1234ABC', '1234BCD']
 places = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15']
 
-# def check_disability(): ??
 
-
-# Clases del Sistema
-class Plaza:
-    def __init__(self, plaza_id, size="medium", is_disabled=False):
-        self.plaza_id = plaza_id
+# System classes
+class Place:
+    def __init__(self, place_id, size="medium", is_disabled=False):
+        self.place_id = place_id
         self.size = size
         self.is_disabled = is_disabled
         self.is_occupied = False
@@ -21,7 +19,7 @@ class Plaza:
 
     def occupy(self, vehicle):
         if self.is_occupied:
-            raise ValueError(f"Plaza {self.plaza_id} ya está ocupada")
+            raise ValueError(f"Plaza {self.place_id} ya está ocupada")
         self.is_occupied = True
         self.vehicle = vehicle
 
@@ -38,43 +36,41 @@ class Vehicle:
         self.assigned_place = assigned_place
 
 
-class Aparcamiento:
-    def __init__(self, num_plazas, num_columnas=6):
-        self.num_columnas = num_columnas
-        self.plazas = [
-            Plaza(f"P{i+1}", size="medium" if i % 3 != 0 else "large", is_disabled=(i < 3)) for i in range(num_plazas)
+class Parking:
+    def __init__(self, num_places, column_number=6):
+        self.column_number = column_number
+        self.places = [
+            Place(f"P{i+1}", size="medium" if i % 3 != 0 else "large", is_disabled=(i < 3)) for i in range(num_places)
         ]
         self.vehicles = []
 
 
 # Configuración inicial
-aparcamiento = Aparcamiento(15)
+parking = Parking(15)
 
 
 class ParkingGUI:
     def __init__(self, root):
         #Initial window configuration
         self.root = root
-        self.root.title("Aparcamiento Inteligente")
+        self.root.title("parking Inteligente")
         self.root.geometry("800x900+20+20")
         self.root.maxsize(width=1200, height=750)
         self.root.minsize(width=1200, height=750) 
 
-        # Crear un canvas para el aparcamiento
+        # Crear un canvas para el parking
         self.canvas = tk.Canvas(self.root, width=1000, height=600, bg="gray")
         self.canvas.grid(column=0, row=0, padx=100)
 
-        self.contenedor = tk.Frame(self.root, width=1000, height=100, bg='lightblue')
-        self.contenedor.grid(column=0, row=1, pady=25)
+        self.button_container = tk.Frame(self.root, width=1000, height=100, bg='lightblue')
+        self.button_container.grid(column=0, row=1, pady=25)
 
         #Command del boton 1 llama a la función que añade un coche al parking
-        self.add_car_button = tk.Button(self.contenedor, text='Add Car', command=self.generate_car).grid(column=0, row=0, padx=(200, 100), pady=(20, 20)) 
-        self.boton2 = tk.Button(self.contenedor, text='+1 Floor').grid(column=1, row=0, padx=10)
-        self.boton3 = tk.Button(self.contenedor, text='-1 Floor').grid(column=2, row=0, padx=10)
+        self.add_car_button = tk.Button(self.button_container, text='Add Car', command=self.generate_car).grid(column=0, row=0, padx=(200, 100), pady=(20, 20)) 
 
 
-        # Dibujar aparcamiento
-        self.num_columnas = 5
+        # Dibujar parking
+        self.column_number = 5
         self.draw_parking()
 
     def draw_parking(self):
@@ -87,14 +83,14 @@ class ParkingGUI:
         global places
 
         self.plaza_coords = {}
-        for i, plaza in enumerate(aparcamiento.plazas):
+        for i, plaza in enumerate(parking.places):
             color = "blue" if plaza.is_disabled else "green"
             self.plaza_coords[places[i]] = (x, y, x + width_plaza, y + height_plaza)
-            self.canvas.create_rectangle(x, y, x + width_plaza, y + height_plaza, fill=color, outline="white", width=4, tags=plaza.plaza_id)
-            self.canvas.create_text(x + width_plaza / 2, y + height_plaza / 2, text=plaza.plaza_id, font=("Arial", 10), fill="white")
+            self.canvas.create_rectangle(x, y, x + width_plaza, y + height_plaza, fill=color, outline="white", width=4, tags=plaza.place_id)
+            self.canvas.create_text(x + width_plaza / 2, y + height_plaza / 2, text=plaza.place_id, font=("Arial", 10), fill="white")
 
             x += width_plaza + horizontal_gap
-            if (i + 1) % self.num_columnas == 0:
+            if (i + 1) % self.column_number == 0:
                 x = 150  # Volver al margen izquierdo
                 y += height_plaza + vertical_gap  # Salto de fila con espacio vertical
 
